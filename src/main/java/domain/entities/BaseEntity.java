a02931602577179b1779909fd7f04d2840781018
georgiilievvv <joroilievvv@gmail.com>
2019-01-31 13:05:34 +0200

    Fixed maven dependencies and the Product entity's id

--- src/main/java/domain/entities/BaseEntity.java
+++ src/main/java/domain/entities/BaseEntity.java
@@ -17,8 +17,8 @@ public abstract class BaseEntity {
     }
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "uuid-string")
+    @GenericGenerator(name = "uuid-string", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", nullable = false, unique = true, updatable = false)
     public String getId() {
         return id;
--- src/main/java/web/IndexServlet.java
+++ src/main/java/web/IndexServlet.java
@@ -5,24 +5,23 @@ import util.HtmlReader;
 import util.ModelMapper;
 
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/")
-public class indexServlet extends HttpServlet {
+@WebServlet("/home")
+public class IndexServlet extends HttpServlet {
 
-    private final static String HTML_INDEX_FILE_PATH = "";
+    private final static String HTML_INDEX_FILE_PATH = "C:\\Users\\joroi\\Desktop\\SoftUni\\JavaWeb\\servletAPI\\src\\main\\resources\\views\\index.html";
 
     private final ModelMapper modelMapper;
     private final HtmlReader htmlReader;
     private final ProductService productService;
 
     @Inject
-    public indexServlet(ModelMapper modelMapper, HtmlReader htmlReader, ProductService productService) {
+    public IndexServlet(ModelMapper modelMapper, HtmlReader htmlReader, ProductService productService) {
         this.modelMapper = modelMapper;
         this.htmlReader = htmlReader;
         this.productService = productService;
@@ -30,7 +29,6 @@ public class indexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
         resp.getWriter().println(this.htmlReader.readFile(HTML_INDEX_FILE_PATH));
     }
 }
